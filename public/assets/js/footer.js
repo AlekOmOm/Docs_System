2d6917e6131d1d6ce20287c0fd285d2562c0d4a2
AlekOmOm <alekellegard@protonmail.com>
2025-03-20 22:07:16 +0100

    added MIT License in footer

--- public/assets/js/footer.js
+++ public/assets/js/footer.js
@@ -1,4 +1,6 @@
 const copyrightYearP = document.getElementById('footer-copyright-year');
+const license = document.getElementById('license');
 
 const currentYear = new Date().getFullYear();
-copyrightYearP.textContent = '©' + currentYear;
+copyrightYearP.textContent = 'AlekOmOm ©' + currentYear;
+license.textContent = 'MIT License';
